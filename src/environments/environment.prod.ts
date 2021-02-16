b6e75fde1fd1d5917fe2b604b38bfadeadda323c
harshitmalhotra02 <54781704+harshitmalhotra02@users.noreply.github.com>
2021-02-16 10:41:54 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  client_id: '',
-  client_secret: '',
+  client_id: 'C44a6d71dfc8245cc215a61a73cabad81e3dae5f8d254056188ccc3e062f5f387',
+  client_secret: '1df5f5a0ac4f40bdbc29c07cb7d6bc723f1444acef3bce07e3c55f674285e395',
   redirect_uri: 'http://localhost:4200/home',
   scope: 'spark:all spark:kms'
 };
